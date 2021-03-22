8fc873c9d91133f26d199565e2a853759614097b
Misanthrope <carrion.30.11@gmail.com>
2021-03-22 11:00:56 +0100

    TicTacToe

--- app/src/main/java/pollub/ism/myapplication/MainActivity.java
+++ app/src/main/java/pollub/ism/myapplication/MainActivity.java
@@ -9,29 +9,16 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    char[] arr = {'O', 'X'};
-    Button[] button = new Button[9];
-    boolean hasWinner = false;
-    boolean hasAnyEmptyField = false;
-    TicTacToe ticTacToe = new TicTacToe(this);
+
+    TicTacToe ticTacToe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ticTacToe = new TicTacToe(this);
     }
 
-    public char[] checkButtons() {
-        char[] getBoardSigns = new char[9];
-
-        for (int i = 1; i <= 9; i++) {
-            int id = getResources().getIdentifier("button" + i, "id", getPackageName());
-            button[i - 1] = (Button) findViewById(id);
-            if (!button[i - 1].getText().equals(""))
-            getBoardSigns[i - 1] = button[i - 1].getText().charAt(0);
-        }
-        return getBoardSigns;
-    }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
@@ -39,12 +26,12 @@ public class MainActivity extends AppCompatActivity {
         char[] getSigns = (char[]) savedInstanceState.getSerializable("boardSigns");
         for (int i = 1; i <= 9; i++) {
             int id = getResources().getIdentifier("button" + i, "id", getPackageName());
-            button[i - 1] = (Button) findViewById(id);
+            ticTacToe.button[i - 1] = (Button) findViewById(id);
             if ((String.valueOf(getSigns[i - 1]).equals("X") || String.valueOf(getSigns[i - 1]).equals("O"))) {
-                button[i - 1].setEnabled(false);
-                button[i - 1].setText(String.valueOf(getSigns[i - 1]));
+                ticTacToe.button[i - 1].setEnabled(false);
+                ticTacToe.button[i - 1].setText(String.valueOf(getSigns[i - 1]));
             } else {
-                button[i - 1].setText("");
+                ticTacToe.button[i - 1].setText("");
             }
 
         }
@@ -55,77 +42,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putSerializable("boardSigns", checkButtons());
+        outState.putSerializable("boardSigns", ticTacToe.checkButtons());
         outState.putInt("numberOfMoves", TicTacToe.moves);
     }
 
 
     public void clickButton(View view) throws InterruptedException {
         int whoseTurn = TicTacToe.moves % 2;
-        Button button1 = findViewById(view.getId());
-        button1.setEnabled(false);
-        button1.setText(String.valueOf(arr[whoseTurn]));
+        ticTacToe.drawSign(view, whoseTurn);
+        ticTacToe.checkWin();
+        ticTacToe.detectedWinOrDrawAction();
         TicTacToe.moves++;
-
-
-        for (int i = 1; i <= 9; i++) {
-                int id = getResources().getIdentifier("button" + i, "id", getPackageName());
-                 button[i - 1] = (Button) findViewById(id);
-        }
-
-
-        for (int i = 0; i < 3; i++) {
-            if (!button[i].getText().equals("")) {
-                if (button[i].getText().equals(button[i + 3].getText()) && button[i + 3].getText().equals(button[i + 6].getText())) {
-                    hasWinner = true;
-                    break;
-                }
-            }
-        }
-
-        for (int i = 0; i < 7; i += 3) {
-            if (!button[i].getText().equals("")) {
-                if (button[i].getText().equals(button[i + 1].getText()) && button[i + 1].getText().equals(button[i + 2].getText())) {
-                    hasWinner = true;
-                    break;
-                }
-            }
-        }
-
-        if (!button[0].getText().equals("")) {
-            if (button[0].getText().equals(button[4].getText()) && button[0].getText().equals(button[8].getText())) {
-                hasWinner = true;
-            }
-        }
-         if (!button[2].getText().equals("")) {
-             if (button[2].getText().equals(button[4].getText()) && button[4].getText().equals(button[6].getText())) {
-                 hasWinner = true;
-            }
-        }
-
-
-        if (hasWinner) {
-            Toast.makeText(this,"Wygrały " + arr[whoseTurn], Toast.LENGTH_LONG).show();
-            for (Button buttons: button) {
-                buttons.setText("");
-                buttons.setEnabled(true);
-            }
-            hasWinner = false;
-        } else {
-            for (Button buttons: button) {
-                if(buttons.getText().equals("")) {
-                    return;
-                }
-            }
-            if (!(hasAnyEmptyField)) {
-                Toast.makeText(this," MAMY REMIS!", Toast.LENGTH_LONG).show();
-                for (Button buttons: button) {
-                    buttons.setText("");
-                    buttons.setEnabled(true);
-                }
-            }
-        }
-
 
     }
 
--- app/src/main/java/pollub/ism/myapplication/TicTacToe.java
+++ app/src/main/java/pollub/ism/myapplication/TicTacToe.java
@@ -3,15 +3,104 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class TicTacToe {
-
-    static int moves = 0;
+    Button[] button;
     Activity activity;
+    static int moves = 0;
+    char[] arr = {'O', 'X'};
+    boolean hasWinner = false;
+    boolean hasAnyEmptyField = false;
 
     public TicTacToe(Activity activity) {
         this.activity = activity;
+        this.button = new Button[9];
+
+        for (int i = 1; i < 9; i++) {
+            int id = this.activity.getResources().getIdentifier("button" + i, "id", this.activity.getPackageName());
+            button[i - 1] = (Button) activity.findViewById(id);
+        }
+
     }
+
+    public char[] checkButtons() {
+        char[] getBoardSigns = new char[9];
+
+        for (int i = 1; i <= 9; i++) {
+            int id = this.activity.getResources().getIdentifier("button" + i, "id", this.activity.getPackageName());
+            button[i - 1] = (Button) this.activity.findViewById(id);
+            if (!button[i - 1].getText().equals(""))
+                getBoardSigns[i - 1] = button[i - 1].getText().charAt(0);
+        }
+        return getBoardSigns;
+    }
+
+    public void checkWin() {
+
+        for (int i = 0; i < 3; i++) {
+            if (!button[i].getText().equals("")) {
+                if (button[i].getText().equals(button[i + 3].getText()) && button[i + 3].getText().equals(button[i + 6].getText())) {
+                    hasWinner = true;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < 7; i += 3) {
+            if (!button[i].getText().equals("")) {
+                if (button[i].getText().equals(button[i + 1].getText()) && button[i + 1].getText().equals(button[i + 2].getText())) {
+                    hasWinner = true;
+                    break;
+                }
+            }
+        }
+
+        if (!button[0].getText().equals("")) {
+            if (button[0].getText().equals(button[4].getText()) && button[0].getText().equals(button[8].getText())) {
+                hasWinner = true;
+            }
+        }
+        if (!button[2].getText().equals("")) {
+            if (button[2].getText().equals(button[4].getText()) && button[4].getText().equals(button[6].getText())) {
+                hasWinner = true;
+            }
+        }
+    }
+
+    public void drawSign(View view, int whoseTurn) {
+        Button button1 = this.activity.findViewById(view.getId());
+        button1.setEnabled(false);
+        button1.setText(String.valueOf(this.arr[whoseTurn]));
+    }
+
+    public void detectedWinOrDrawAction() {
+
+        if (hasWinner) {
+           Toast.makeText(this.activity, "Wygral " + arr[TicTacToe.moves % 2], Toast.LENGTH_LONG).show();
+            for (Button buttons: button) {
+                buttons.setText("");
+                buttons.setEnabled(true);
+            }
+            hasWinner = false;
+        } else {
+            for (Button buttons: button) {
+                if(buttons.getText().equals("")) {
+                    return;
+                }
+            }
+            if (!(hasAnyEmptyField)) {
+                Toast.makeText(this.activity," MAMY REMIS!" , Toast.LENGTH_LONG).show();
+                for (Button buttons: button) {
+                    buttons.setText("");
+                    buttons.setEnabled(true);
+                }
+            }
+        }
+
+    }
+
 
 
 
