28c40875545afffd7dc85e163244610da9613e3c
Misanthrope <carrion.30.11@gmail.com>
2021-03-22 08:23:30 +0100

    TicTacToe

---
+++ app/src/main/java/pollub/ism/myapplication/TicTacToe.java
@@ -0,0 +1,18 @@
+package pollub.ism.myapplication;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.view.View;
+
+public class TicTacToe {
+
+    static int moves = 0;
+    Activity activity;
+
+    public TicTacToe(Activity activity) {
+        this.activity = activity;
+    }
+
+
+
+}
